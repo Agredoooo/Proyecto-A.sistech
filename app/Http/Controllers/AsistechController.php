69dd3db1e5e6143a4d2b1572c89f2e7333b7c9cb
Marco Agredo <marcoagredo33@gmail.com>
2024-06-05 12:25:54 -0500

    "se"

---
+++ app/Http/Controllers/AsistechController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AsistechController extends Controller
+{
+    //
+
+    public function create()
+    {
+        return "usuario";
+    }
+}
